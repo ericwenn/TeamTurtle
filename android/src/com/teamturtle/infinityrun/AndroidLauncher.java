7945ab4fc4eb3ddcb426673dbe58ebe010be4ccd
Eric Wennerberg <ericwenn@gmail.com>
2016-09-19 08:22:28 +0200

    LibGDX init

---
+++ android/src/com/teamturtle/infinityrun/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.teamturtle.infinityrun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.teamturtle.infinityrun.InfinityRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new InfinityRun(), config);
+	}
+}
