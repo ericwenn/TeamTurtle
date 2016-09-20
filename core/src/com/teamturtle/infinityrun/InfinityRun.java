df6c1aa69635d592bdffb4cea3056f273e3559dc
Eric Wennerberg <ericwenn@gmail.com>
2016-09-20 09:36:06 +0200

    InifnityRun main game and first screen

--- core/src/com/teamturtle/infinityrun/InfinityRun.java
+++ core/src/com/teamturtle/infinityrun/InfinityRun.java
@@ -1,33 +1,24 @@
 package com.teamturtle.infinityrun;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class InfinityRun extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class InfinityRun extends Game {
+
+	private SpriteBatch mSpriteBatch;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		mSpriteBatch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		mSpriteBatch.dispose();
 	}
 }
---
+++ core/src/com/teamturtle/infinityrun/screens/GameScreen.java
@@ -0,0 +1,45 @@
+package com.teamturtle.infinityrun.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by ericwenn on 9/20/16.
+ */
+public class GameScreen implements Screen {
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
