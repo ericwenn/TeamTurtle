ef8c0366babde9d8b0b105d080b0a26832b62957
Eric Wennerberg <ericwenn@gmail.com>
2016-09-20 10:42:56 +0200

    Player added

--- core/src/com/teamturtle/infinityrun/screens/GameScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/GameScreen.java
@@ -8,15 +8,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.teamturtle.infinityrun.InfinityRun;
+import com.teamturtle.infinityrun.sprites.Player;
 
 /**
  * Created by ericwenn on 9/20/16.
  */
 public class GameScreen implements Screen {
+    public static final int GAME_SPEED = 50;
+
     private OrthographicCamera cam;
     private SpriteBatch mSpriteBatch;
     private Texture bg;
     private FillViewport mFillViewport;
+    private Player mPlayer;
 
     public GameScreen( SpriteBatch mSpriteBatch ) {
         this.mSpriteBatch = mSpriteBatch;
@@ -25,6 +29,8 @@ public class GameScreen implements Screen {
 
         this.cam.position.set(mFillViewport.getWorldWidth() / 2, mFillViewport.getWorldHeight() / 2, 0);
         this.bg = new Texture("bg.jpg");
+
+        this.mPlayer = new Player( new Texture("badlogic.jpg"));
     }
 
 
@@ -33,17 +39,20 @@ public class GameScreen implements Screen {
 
     }
 
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        this.cam.position.add(1, 0, 0);
+        mPlayer.update(delta);
+        this.cam.position.add( GAME_SPEED * delta, 0, 0);
         cam.update();
         mSpriteBatch.setProjectionMatrix(cam.combined);
         mSpriteBatch.begin();
 
         mSpriteBatch.draw(bg, 0, 0, 800, 480);
+        mSpriteBatch.draw( mPlayer, mPlayer.getX(), mPlayer.getY());
         mSpriteBatch.end();
     }
 
---
+++ core/src/com/teamturtle/infinityrun/sprites/Player.java
@@ -0,0 +1,22 @@
+package com.teamturtle.infinityrun.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.teamturtle.infinityrun.InfinityRun;
+import com.teamturtle.infinityrun.screens.GameScreen;
+
+/**
+ * Created by ericwenn on 9/20/16.
+ */
+public class Player extends Sprite {
+
+    public Player(Texture t) {
+        super( t, 16, 16);
+        setPosition(100, InfinityRun.HEIGHT / 2);
+    }
+
+
+    public void update(float dt) {
+        setPosition( getX() + GameScreen.GAME_SPEED * dt, getY());
+    }
+}
