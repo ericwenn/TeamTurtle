b9b6b5584c06b0dbcc133a8651e2393113ac5107
Eric Wennerberg <ericwenn@gmail.com>
2016-09-20 09:16:11 +0200

    libgdx init with desktop + ios

---
+++ desktop/src/com/teamturtle/infinityrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.teamturtle.infinityrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.teamturtle.infinityrun.InfinityRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InfinityRun(), config);
+	}
+}
---
+++ ios/src/com/teamturtle/infinityrun/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.teamturtle.infinityrun;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.teamturtle.infinityrun.InfinityRun;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new InfinityRun(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
