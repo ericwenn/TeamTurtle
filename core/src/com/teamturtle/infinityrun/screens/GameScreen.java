86dedf1f40f3852578a6e2180fe78a14b5fa389c
Eric Wennerberg <ericwenn@gmail.com>
2016-09-20 10:23:14 +0200

    first userstory

--- core/src/com/teamturtle/infinityrun/screens/GameScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/GameScreen.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.teamturtle.infinityrun.InfinityRun;
 
 /**
  * Created by ericwenn on 9/20/16.
@@ -14,10 +16,14 @@ public class GameScreen implements Screen {
     private OrthographicCamera cam;
     private SpriteBatch mSpriteBatch;
     private Texture bg;
+    private FillViewport mFillViewport;
 
     public GameScreen( SpriteBatch mSpriteBatch ) {
         this.mSpriteBatch = mSpriteBatch;
-        this.cam = new OrthographicCamera();
+        this.mFillViewport = new FillViewport(InfinityRun.WIDTH, InfinityRun.HEIGHT);
+        this.cam = new OrthographicCamera( mFillViewport.getWorldWidth(), mFillViewport.getWorldHeight());
+
+        this.cam.position.set(mFillViewport.getWorldWidth() / 2, mFillViewport.getWorldHeight() / 2, 0);
         this.bg = new Texture("bg.jpg");
     }
 
@@ -32,7 +38,11 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        this.cam.position.add(1, 0, 0);
+        cam.update();
+        mSpriteBatch.setProjectionMatrix(cam.combined);
         mSpriteBatch.begin();
+
         mSpriteBatch.draw(bg, 0, 0, 800, 480);
         mSpriteBatch.end();
     }
@@ -61,6 +71,6 @@ public class GameScreen implements Screen {
     public void dispose() {
         mSpriteBatch.dispose();
         bg.dispose();
-        
+
     }
 }
