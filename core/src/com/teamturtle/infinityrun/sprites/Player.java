571ca051e984fb43e2d31afbc903731da564f3fc
Eric Wennerberg <ericwenn@gmail.com>
2016-09-22 13:56:00 +0200

    SUpergravity

--- core/src/com/teamturtle/infinityrun/sprites/Player.java
+++ core/src/com/teamturtle/infinityrun/sprites/Player.java
@@ -2,6 +2,7 @@ package com.teamturtle.infinityrun.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.teamturtle.infinityrun.InfinityRun;
 import com.teamturtle.infinityrun.screens.GameScreen;
 
@@ -10,13 +11,24 @@ import com.teamturtle.infinityrun.screens.GameScreen;
  */
 public class Player extends Sprite {
 
+    private Vector2 velo;
+
     public Player(Texture t) {
         super( t );
+        velo = new Vector2( GameScreen.GAME_SPEED, -80);
+
         setPosition(100, InfinityRun.HEIGHT / 2);
     }
 
 
+
+
     public void update(float dt) {
-        setPosition( getX() + GameScreen.GAME_SPEED * dt, getY());
+        // Update x
+        setPosition( getX() + velo.x, getY() );
+
+        // Update y
+        float y = 
+        setPosition( getX(), getY() + velo.y * dt);
     }
 }
