3437d761fab67ce2482f510691ae434cf50d1ab0
Eric Wennerberg <ericwenn@gmail.com>
2016-10-02 15:19:39 +0200

    MissionStage table

--- core/src/com/teamturtle/infinityrun/stages/MissionStage.java
+++ core/src/com/teamturtle/infinityrun/stages/MissionStage.java
@@ -1,6 +1,9 @@
 package com.teamturtle.infinityrun.stages;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.teamturtle.infinityrun.InfinityRun;
 
@@ -11,5 +14,11 @@ public class MissionStage extends Stage {
 
     public MissionStage() {
         super( new FillViewport(InfinityRun.WIDTH, InfinityRun.HEIGHT));
+
+
+        Table missionTable = new Table(new Skin(Gdx.files.internal("skin/uiskin.json")));
+
+        missionTable.
+        this.addActor(missionTable);
     }
 }
