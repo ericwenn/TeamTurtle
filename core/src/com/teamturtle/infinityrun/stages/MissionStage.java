3b6e45fe979ff5ed0b391df3c9a9175c7ce906c4
Eric Wennerberg <ericwenn@gmail.com>
2016-10-02 15:44:36 +0200

    Use EmojiModel to print the missionTable

--- core/src/com/teamturtle/infinityrun/stages/MissionStage.java
+++ core/src/com/teamturtle/infinityrun/stages/MissionStage.java
@@ -4,32 +4,48 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.teamturtle.infinityrun.InfinityRun;
+import com.teamturtle.infinityrun.models.EmojiModel;
 
 /**
  * Created by ericwenn on 10/1/16.
  */
 public class MissionStage extends Stage {
 
-    private static final int TABLE_WIDTH = 100,
+    private static final int TABLE_WIDTH = 200,
             TABLE_HEIGHT = 100;
+    private Skin mSkin;
 
+    private Table mMissionTable;
     public MissionStage() {
         super( new FillViewport(InfinityRun.WIDTH, InfinityRun.HEIGHT));
 
 
-        Table missionTable = new Table(new Skin(Gdx.files.internal("skin/uiskin.json")));
-        missionTable.setSize(TABLE_WIDTH, TABLE_HEIGHT);
-        missionTable.setPosition( 0, InfinityRun.HEIGHT - TABLE_HEIGHT);
+        mSkin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+        mMissionTable = new Table(mSkin);
+        mMissionTable.setSize(TABLE_WIDTH, TABLE_HEIGHT);
+        mMissionTable.setPosition( 0, InfinityRun.HEIGHT - TABLE_HEIGHT);
 
 
-        Image emojiImage = new Image(new Texture(Gdx.files.internal("emoji/1f34e.png")));
+        // TODO get emojimodel from mission
+        EmojiModel emojiModel = new EmojiModel("Äpple", "audio/apple.wav", "emoji/1f34e.png");
+        insertEmoji(emojiModel);
+
+        this.addActor(mMissionTable);
+    }
+
+
+    private void insertEmoji(EmojiModel emojiModel) {
+        Image emojiImage = new Image(new Texture(Gdx.files.internal(emojiModel.getIconUrl())));
         emojiImage.setScaling(Scaling.fit);
-        missionTable.add(emojiImage).height(50);
-        this.addActor(missionTable);
+        mMissionTable.add(emojiImage).height(50);
+
+        Label emojiLabel = new Label( emojiModel.getEmojiName(), mSkin);
+        mMissionTable.add(emojiLabel).height(50);
     }
 }
