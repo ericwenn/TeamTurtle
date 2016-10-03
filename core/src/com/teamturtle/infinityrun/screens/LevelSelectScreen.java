02b986f629f93769e8e6eb18b1d8cb40fd1dd2a1
Henrik Boström <ostmoz@gmail.com>
2016-10-03 09:16:19 +0200

    Added images for levelscreen

---
+++ core/src/com/teamturtle/infinityrun/screens/LevelSelectScreen.java
@@ -0,0 +1,22 @@
+package com.teamturtle.infinityrun.screens;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Henrik on 2016-10-03.
+ */
+public class LevelSelectScreen extends AbstractScreen{
+
+    private Stage stage;
+
+    public LevelSelectScreen(SpriteBatch spriteBatch) {
+        super(spriteBatch);
+
+    }
+
+    @Override
+    public void buildStage() {
+
+    }
+}
