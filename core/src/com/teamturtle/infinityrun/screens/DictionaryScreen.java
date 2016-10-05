2e2b444a08b0a5a4fe8818aa40a3190678860081
Henrik Boström <ostmoz@gmail.com>
2016-10-05 18:02:27 +0200

    The dictionary is done and gets word data from playerdata

--- core/src/com/teamturtle/infinityrun/screens/DictionaryScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/DictionaryScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -20,6 +21,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.teamturtle.infinityrun.InfinityRun;
 import com.teamturtle.infinityrun.PathConstants;
@@ -36,6 +38,13 @@ import java.util.List;
  * Created by Henrik on 2016-10-03.
  */
 public class DictionaryScreen extends AbstractScreen {
+
+    private static final String LABEL_BG_URL = "label_bg.png";
+    private static final String LABEL_TEXT = "Ord hittade:";
+    private static final String LABEL_UNKNOWN = "???";
+    private static final float PAD_ROW = 20f;
+    private static final float PAD_SCROLL = 5f;
+    private static final float GRID_COLUMN_WIDTH = 4;
 
     private Stage stage;
     private IScreenObserver observer;
@@ -54,12 +63,12 @@ public class DictionaryScreen extends AbstractScreen {
         skin.addRegions(new TextureAtlas(Gdx.files.internal("skin/uiskin.atlas")));
         skin.load(Gdx.files.internal("skin/uiskin.json"));
 
-        createBackButton();
-        createGridTable();
+        initBackButton();
+        initGridTable();
 
     }
 
-    private void createBackButton() {
+    private void initBackButton() {
         imageButton = new ImageButton(skin, "back_button");
         imageButton.addListener(new ChangeListener() {
             @Override
@@ -71,42 +80,69 @@ public class DictionaryScreen extends AbstractScreen {
                 }
             }
         });
+        imageButton.pad(10f);
     }
 
-    private void createGridTable() {
+
+    private void initGridTable() {
         WordLoader wordLoader = new WordLoader();
         PlayerData playerData = new PlayerData();
 
         Table grid = new Table();
 
-
-        /*for(int i = 0; i < emojis.size(); i++) {
-            grid.add(createGridItem(emojis.get(i)));
-            if ((i+1) % 4 == 0) {
-                grid.row();
+        int collectedWordsAmount = 0;
+        List<Word> allWords = wordLoader.getAllWords();
+        for(int i = 0; i < allWords.size(); i++){
+            Word word = allWords.get(i);
+            if (playerData.hasPlayerCollectedWord(word)) {
+                grid.add(createGridItem(word, false));
+                collectedWordsAmount++;
+            }else{
+                grid.add(createGridItem(word, true));
             }
-        }*/
+            if ((i+1) % GRID_COLUMN_WIDTH == 0) {
+                grid.row().padTop(PAD_ROW);
+            }
+        }
 
-        ScrollPane scroller = new ScrollPane(grid, skin);
+        ScrollPane scroller = new ScrollPane(grid);
         scroller.setForceScroll(false, true);
         Table rootTable = new Table();
         rootTable.setFillParent(true);
-        rootTable.add(imageButton).left().top();
-        rootTable.add(scroller).fill().expand().pad(5f);
+
+        Table leftPanel = new Table();
+        leftPanel.add(imageButton);
+        leftPanel.row();
+        Table labelBg = new Table();
+        labelBg.add(new Label(
+                 LABEL_TEXT + "\n" + collectedWordsAmount + " / " + allWords.size(), skin));
+        labelBg.setBackground(
+                new TextureRegionDrawable(new TextureRegion(new Texture(LABEL_BG_URL))));
+        leftPanel.add(labelBg);
+        rootTable.add(leftPanel).left().top();
+        rootTable.add(scroller).fill().expand().pad(PAD_SCROLL);
         stage.addActor(rootTable);
     }
 
-    private Table createGridItem(Word word) {
-        Table gridItem = new Table();
-        System.out.println(word.getIconUrl());
-        gridItem.add(new Image(new Texture(word.getIconUrl())));
+    private Table createGridItem(final Word word, boolean tinted) {
+        final Table gridItem = new Table();
+        Image image = new Image(new Texture(word.getIconUrl()));
+        Label label;
+        if (tinted) {
+            image.setColor(Color.BLACK);
+            label = new Label(LABEL_UNKNOWN, skin);
+        }else{
+            label = new Label(word.getText(), skin);
+        }
+        gridItem.add(image);
         gridItem.row();
-        gridItem.add(new Label(word.getText(), skin));
+        gridItem.add(label);
         gridItem.setTouchable(Touchable.enabled);
         gridItem.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                //TODO changeScreen(id, emoji)
+                //TODO change screen
+                //showWordScreen(word);
             }
         });
         return gridItem;
