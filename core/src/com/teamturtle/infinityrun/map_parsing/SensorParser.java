8448d2140e602fbc625c49fea9eae9a00ddf9a71
Alfred Björk <alfbjo@student.chalmers.se>
2016-10-05 16:29:54 +0200

    Collision with polygon objects

--- core/src/com/teamturtle/infinityrun/map_parsing/SensorParser.java
+++ core/src/com/teamturtle/infinityrun/map_parsing/SensorParser.java
@@ -1,8 +1,11 @@
 package com.teamturtle.infinityrun.map_parsing;
 
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -51,7 +54,8 @@ public class SensorParser implements MapParser {
         FixtureDef fdef = new FixtureDef();
 
 
-        for (MapObject object : tiledMap.getLayers().get(layerName).getObjects().getByType(RectangleMapObject.class)) {
+        // Rectangular objects
+        /*for (MapObject object : tiledMap.getLayers().get(layerName).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
             bdef.type = BodyDef.BodyType.StaticBody;
             bdef.position.set(((rect.getX() + rect.getWidth() / 2) / InfinityRun.PPM)
@@ -63,6 +67,74 @@ public class SensorParser implements MapParser {
             fdef.shape = shape;
             fdef.isSensor = true;
             body.createFixture(fdef).setUserData(layerName);
+        }
+
+        // Polygon shapes
+        for (MapObject object : tiledMap.getLayers().get(layerName).getObjects().getByType(PolygonMapObject.class)) {
+            Polygon pol = ((PolygonMapObject) object).getPolygon();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+
+            bdef.position.set(((pol.getX() + pol.getBoundingRectangle().getWidth() / 2) / InfinityRun.PPM)
+                    , ((pol.getY() + pol.getBoundingRectangle().getHeight() / 2) / InfinityRun.PPM));
+
+            Body body = world.createBody(bdef);
+
+            shape.set(pol.getVertices());
+
+            fdef.shape = shape;
+            fdef.isSensor = true;
+            body.createFixture(fdef).setUserData(layerName);
+        }*/
+
+        for (MapObject object : tiledMap.getLayers().get(layerName).getObjects()) {
+            if (object instanceof RectangleMapObject) {
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set(((rect.getX() + rect.getWidth() / 2) / InfinityRun.PPM)
+                        , ((rect.getY() + rect.getHeight() / 2) / InfinityRun.PPM));
+                Body body = world.createBody(bdef);
+
+                shape.setAsBox((rect.getWidth() / 2) / InfinityRun.PPM
+                        , (rect.getHeight() / 2) / InfinityRun.PPM);
+                fdef.shape = shape;
+                fdef.isSensor = true;
+                body.createFixture(fdef).setUserData(layerName);
+            }
+
+            if (object instanceof PolygonMapObject) {
+
+                Polygon pol = ((PolygonMapObject) object).getPolygon();
+
+                float vertices[] = pol.getTransformedVertices();
+
+                for (int x = 0; x < vertices.length; x++) {
+                    vertices[x] /= InfinityRun.PPM;
+                }
+
+                bdef.type = BodyDef.BodyType.StaticBody;
+                bdef.position.set(((pol.getBoundingRectangle().getX() + pol.getBoundingRectangle().getWidth() / 2) / InfinityRun.PPM)
+                        , ((pol.getBoundingRectangle().getY() + pol.getBoundingRectangle().getHeight() / 2) / InfinityRun.PPM));
+                Body body = world.createBody(bdef);
+
+                shape.set(vertices);
+                fdef.shape = shape;
+                fdef.isSensor = true;
+                body.createFixture(fdef).setUserData(layerName);
+            }
+
+            /*if (object instanceof PolylineMapObject) {
+                float vertices[] = ((PolylineMapObject) object).getPolyline()
+                        .getTransformedVertices();
+                for (int x = 0; x < vertices.length; x++) {
+                    vertices[x] /= InfinityRun.PPM;
+                }
+                ChainShape shape2 = new ChainShape();
+                shape2.createChain(vertices);
+                def.position.set(0, 0);
+                cuerpos.add(world.createBody(def));
+                cuerpos.get(cuerpos.size - 1).createFixture(shape2, 0);
+            }*/
         }
     }
 
