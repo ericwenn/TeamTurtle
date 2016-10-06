1d41625b18b179fc979abca498dd152a35fa5dfc
Eric Wennerberg <ericwenn@gmail.com>
2016-10-06 13:45:15 +0200

    Hotfix: number of stars required to start next level

--- core/src/com/teamturtle/infinityrun/screens/LevelSelectScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/LevelSelectScreen.java
@@ -66,7 +66,7 @@ public class LevelSelectScreen extends AbstractScreen{
         rootTable.setSize(ROOT_TABLE_WIDTH, ROOT_TABLE_HEIGHT);
 
         int i = 1;
-        // If the user has gotten atleast 2 starts on the previous level, they can play this one.
+        // If the user has gotten atleast 1 star on the previous level, they can play this one.
         boolean progressedThisFar = true;
         for(final Level level: levels) {
             int playerScoreOnLevel = mPlayerData.getPlayerProgressOnLevel(level);
@@ -102,7 +102,7 @@ public class LevelSelectScreen extends AbstractScreen{
             if (i % 5 == 0){
                 rootTable.row();
             }
-            progressedThisFar = progressedThisFar && playerScoreOnLevel > 1;
+            progressedThisFar = progressedThisFar && playerScoreOnLevel > 0;
             i++;
         }
         backButton = new ImageButton(skin, "back_button");
