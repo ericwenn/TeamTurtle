f370c20ca6558e641f9572f496504974798f0b6b
RasmusJemth <jemthr@student.chalmers.se>
2016-10-09 15:47:24 +0200

    Double jump

--- core/src/com/teamturtle/infinityrun/sprites/Player.java
+++ core/src/com/teamturtle/infinityrun/sprites/Player.java
@@ -81,11 +81,13 @@ public class Player extends AbstractEntity {
             canDoubleJump = true;
         }
         else if(canDoubleJump){
-            if(b2body.getLinearVelocity().y <= 0)
+            if(b2body.getLinearVelocity().y <= 0) {
                 b2body.setLinearVelocity(b2body.getLinearVelocity().x, 0);
-            else
+            }
+            else {
                 b2body.setLinearVelocity(b2body.getLinearVelocity().x,
                         b2body.getLinearVelocity().y / 2);
+            }
             jumpStrength = JUMP_IMPULSE;
             canDoubleJump = false;
         }
