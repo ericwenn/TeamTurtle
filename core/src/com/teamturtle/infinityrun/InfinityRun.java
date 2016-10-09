4707ed2a6ebb94aa9a93f2cf49148b3df4b77242
Eric Wennerberg <ericwenn@gmail.com>
2016-10-09 11:12:28 +0200

    remove player_tail.png

--- core/src/com/teamturtle/infinityrun/InfinityRun.java
+++ core/src/com/teamturtle/infinityrun/InfinityRun.java
@@ -1,15 +1,11 @@
 package com.teamturtle.infinityrun;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.teamturtle.infinityrun.models.level.Level;
 import com.teamturtle.infinityrun.models.level.LevelDataHandler;
-import com.teamturtle.infinityrun.models.sentences.Sentence;
-import com.teamturtle.infinityrun.models.sentences.SentenceLoader;
 import com.teamturtle.infinityrun.models.words.Word;
-import com.teamturtle.infinityrun.models.words.WordImpl;
 import com.teamturtle.infinityrun.screens.AbstractScreen;
 import com.teamturtle.infinityrun.screens.DictionaryScreen;
 import com.teamturtle.infinityrun.screens.GameScreen;
@@ -20,7 +16,6 @@ import com.teamturtle.infinityrun.screens.StartScreen;
 import com.teamturtle.infinityrun.screens.WordScreen;
 import com.teamturtle.infinityrun.screens.level_end_screens.LostLevelScreen;
 import com.teamturtle.infinityrun.screens.level_end_screens.WonLevelScreen;
-import com.teamturtle.infinityrun.sprites.emoji.Emoji;
 import com.teamturtle.infinityrun.storage.PlayerData;
 
 import java.util.List;
