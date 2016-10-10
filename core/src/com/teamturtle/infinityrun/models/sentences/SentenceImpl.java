c5f4a759dbd73ec11e4acf6473b21801f0f7251f
Eric Wennerberg <ericwenn@gmail.com>
2016-10-10 12:58:07 +0200

    unwritten field

--- core/src/com/teamturtle/infinityrun/models/sentences/SentenceImpl.java
+++ core/src/com/teamturtle/infinityrun/models/sentences/SentenceImpl.java
@@ -4,7 +4,7 @@ package com.teamturtle.infinityrun.models.sentences;
  * Created by ericwenn on 10/5/16.
  */
 public class SentenceImpl implements Sentence {
-    private String text;
+    private String text = null;
 
     public SentenceImpl() {
     }
