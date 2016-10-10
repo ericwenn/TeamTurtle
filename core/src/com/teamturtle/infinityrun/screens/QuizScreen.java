1b566c2ab4e56088dc08bccd51359e99ddfbd67e
David Söderberg <dasovm@gmail.com>
2016-10-10 11:34:13 +0200

    Right/Wrong sound init

--- core/src/com/teamturtle/infinityrun/screens/QuizScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/QuizScreen.java
@@ -1,6 +1,7 @@
 package com.teamturtle.infinityrun.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.teamturtle.infinityrun.PathConstants;
@@ -25,6 +26,7 @@ public class QuizScreen extends AbstractScreen implements IQuizStageListener {
     private IScreenObserver observer;
     private Level level;
     private int score;
+    private Sound rightAnswerSound, wrongAnswerSound;
 
     public QuizScreen(SpriteBatch spriteBatch, IScreenObserver observer, Level level
             , List<Word> collectedWords, int score) {
@@ -34,6 +36,8 @@ public class QuizScreen extends AbstractScreen implements IQuizStageListener {
         this.observer = observer;
         this.level = level;
         this.score = score;
+        rightAnswerSound = Gdx.audio.newSound(Gdx.files.internal("audio/right_answer.wav"));
+        wrongAnswerSound = Gdx.audio.newSound(Gdx.files.internal("audio/wrong_answer.wav"));
     }
 
     @Override
@@ -54,8 +58,10 @@ public class QuizScreen extends AbstractScreen implements IQuizStageListener {
     @Override
     public void onGuessClick(boolean isChoiceRight) {
         if (isChoiceRight) {
+            rightAnswerSound.play();
             observer.levelWon(level, ++score);
         } else {
+            wrongAnswerSound.play();
             observer.levelWon(level, score);
         }
     }
