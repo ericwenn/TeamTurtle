04c1b1202b5338b2215ed4d24751889fdb7574d7
Eric Wennerberg <ericwenn@gmail.com>
2016-10-14 10:39:11 +0200

    Game is played in immersive mode on Android

--- android/src/com/teamturtle/infinityrun/AndroidLauncher.java
+++ android/src/com/teamturtle/infinityrun/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.teamturtle.infinityrun.InfinityRun;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useImmersiveMode = true;
 		initialize(new InfinityRun(), config);
 	}
 }
