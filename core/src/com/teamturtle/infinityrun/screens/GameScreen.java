d91c0164554e43c53df419b61304627f7bc6781a
David Söderberg <dasovm@gmail.com>
2016-10-15 18:10:56 +0200

    Feedback sound when triggerExplode

--- core/src/com/teamturtle/infinityrun/screens/GameScreen.java
+++ core/src/com/teamturtle/infinityrun/screens/GameScreen.java
@@ -2,6 +2,7 @@ package com.teamturtle.infinityrun.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -92,6 +93,7 @@ public class GameScreen extends AbstractScreen implements IPauseStageHandler {
     private OrthographicCamera mFixedCamera;
     private List<Word> possibleWords, collectedWords;
     private WordLoader wordLoader;
+    private Sound completedSound, failureSound;
 
     private Level level;
     private Mission activeMission;
@@ -129,6 +131,8 @@ public class GameScreen extends AbstractScreen implements IPauseStageHandler {
 
         playerData = new PlayerData();
         mJumpAnimations = new JumpAnimations();
+        completedSound = Gdx.audio.newSound(Gdx.files.internal("audio/right_answer.wav"));
+        failureSound = Gdx.audio.newSound(Gdx.files.internal("audio/wrong_answer.wav"));
 
         Gdx.input.setInputProcessor(this);
     }
@@ -324,6 +328,8 @@ public class GameScreen extends AbstractScreen implements IPauseStageHandler {
         for (Entity ent : emojiSprites) {
             ent.dispose();
         }
+        completedSound.dispose();
+        failureSound.dispose();
         mPlayer.dispose();
         bg.dispose();
         pauseStage.dispose();
@@ -403,10 +409,12 @@ public class GameScreen extends AbstractScreen implements IPauseStageHandler {
             public void onCollision(Player p, Emoji e) {
                 e.triggerExplode();
                 if (activeMission.getCorrectWord().equals(e.getWordModel())) {
+                    completedSound.play();
                     mPlayerTail.setColor(SUCCESS_COLOR);
                     mPlayer.setColor(SUCCESS_COLOR);
                     mJumpAnimations.setColor(SUCCESS_COLOR);
                 } else {
+                    failureSound.play();
                     mPlayerTail.setColor(FAILURE_COLOR);
                     mPlayer.setColor(FAILURE_COLOR);
                     mJumpAnimations.setColor(FAILURE_COLOR);
